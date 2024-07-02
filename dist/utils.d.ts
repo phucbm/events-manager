cb840674ee098f46b4b04c6bf7e361401e1f4fd8
phucbm <hiphucbui@gmail.com>
2024-07-02 12:01:13 +0700

    build(publish): remove dist from git, build in workflow

--- dist/utils.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Get validated event name
- * make sure event name always has "on" at the beginning
- * @param name
- * @returns {string}
- */
-export declare function getValidatedEventName(name: string): string;
